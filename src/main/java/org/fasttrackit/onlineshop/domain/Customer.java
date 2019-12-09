d6d593780b5c5681f875b527a124eb5d10704e08
Roland <rolandattila@yahoo.com>
2019-12-09 19:27:19 +0200

    added customer service

---
+++ src/main/java/org/fasttrackit/onlineshop/domain/Customer.java
@@ -0,0 +1,51 @@
+package org.fasttrackit.onlineshop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    private long id;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
